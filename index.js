aa26967de0923a5b970bdd016bbf396df4a518cf
Abel Koech <koechabel161@gmail.com>
2024-07-03 00:29:13 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers,name){
+    return drivers.filter(driver=>driver.toLowerCase()===name.toLowerCase())
+}
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver=>driver.slice(0,letters.length)===letters);
+}
+function matchName(drivers,name){
+return drivers.filter(function(driver){
+    return driver.name===name
+})
+}
